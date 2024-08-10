4e6dcf568afe7c6166de9a35b5aa5f95e030c4fd
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2024-08-10 13:23:34 +0530

    update data

--- web/src/data/UserList.ts
+++ web/src/data/UserList.ts
@@ -1,7 +1,69 @@
+// export const users = [
+//   { id: "1", role: "admin" },
+//   { id: "2", role: "staff" },
+//   { id: "3", role: "staff" },
+//   { id: "4", role: "staff" },
+//   // Add more users as needed
+// ];
+
 export const users = [
-  { id: "1", role: "admin" },
-  { id: "2", role: "staff" },
-  { id: "3", role: "staff" },
-  { id: "4", role: "staff" },
-  // Add more users as needed
+  { id: "1", 
+    role: "admin",
+    name: "User1",
+    email: "user1@example.com",
+    pass: "111",
+    project: [
+      {
+          projectId: "123",
+          projectRole: "admin"
+      },
+      {
+          projectId: "456",
+          projectRole: "admin"
+      }
+    ]
+  },
+  {
+    id: "2", 
+    role: "staff",
+    name: "User2",
+    email: "user2@example.com",
+    pass: "222",
+    project: [
+      {
+          projectId: "456",
+          projectRole: "contributor"
+      }
+    ]
+  },
+  {
+    id: "3", 
+    role: "staff",
+    name: "User3",
+    email: "user3@example.com",
+    pass: "333",
+    project: [
+      {
+          projectId: "123",
+          projectRole: "contributor"
+      },
+      {
+          projectId: "789",
+          projectRole: "contributor"
+      }
+    ]
+  },
+  {
+    id: "4", 
+    role: "staff",
+    name: "User4",
+    email: "user4@example.com",
+    pass: "444",
+    project: [
+      {
+        projectId: "789",
+          projectRole: "reviewer"
+      }
+    ]
+  },
 ];
---
+++ web/src/data/projectList.ts
@@ -0,0 +1,62 @@
+export const projects = [
+    {
+      id: '1',
+      name: 'Web Development',
+      members: {
+        admin: '1',
+        contributor: '2',
+        approver: '3',
+        reviewer: '4',
+      }
+    },
+    {
+      id: '2',
+      name: 'UI/UX Course',
+      members: {
+        admin: '1',
+        contributor: '4',
+        approver: '2',
+        reviewer: '3',
+      }
+    },
+    {
+      id: '3',
+      name: 'AI and Machine Learning',
+      members: {
+        admin: '1',
+        contributor: '3',
+        approver: '4',
+        reviewer: '2',
+      }
+    },
+    // {
+    //   id: '4',
+    //   name: 'DevOps',
+    //   members: {
+    //     admin: '1',
+    //     contributor: '2',
+    //     approver: '3',
+    //     reviewer: '4',
+    //   }
+    // },
+    // {
+    //   id: '5',
+    //   name: 'System Design',
+    //   members: {
+    //     admin: '1',
+    //     contributor: '4',
+    //     approver: '2',
+    //     reviewer: '3',
+    //   }
+    // },
+    // {
+    //   id: '6',
+    //   name: 'Front-End Develop-mental',
+    //   members: {
+    //     admin: '1',
+    //     contributor: '3',
+    //     approver: '4',
+    //     reviewer: '2',
+    //   }
+    // },
+  ];
